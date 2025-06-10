50f3c0363563dfe84269b58de6910d07a02c51e0
LeonBerty29 <leonbertrand29@gmail.com>
2025-06-10 12:08:43 +0100

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,46 @@
+import type { Metadata } from "next";
+// import {
+//   Geist,
+//   Geist_Mono,
+// } from "next/font/google";
+import "./globals.css";
+import { poppins } from "@/fonts";
+import Footer from "@/components/layout/footer";
+
+
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
+
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
+
+
+
+export const metadata: Metadata = {
+  title: "Exclusive Algarve",
+  description: "Trusted luxury real estate brokers in Algarve, Portugal",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${poppins.className} antialiased`}
+      >
+        <main className="w-full">
+          {children}
+        </main>
+
+        <Footer />
+      </body>
+    </html>
+  );
+}
